51f6a0c2faed29659c1b4edbc158758663bf0746
delphinerichard <drichard@enssat.fr>
2020-05-11 21:51:44 +0200

    premier bot discord

--- app.js
+++ app.js
@@ -3,4 +3,20 @@ const app = express();
 
 const PORT = 5000;
 
+
+app.get('/', function(req, res) {
+    res.render('interfaceadmin.ejs');
+});
+
+app.post('/', function(req, res) {
+    res.render('interfaceadmin.ejs');
+});
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
 app.listen(PORT, console.log('Démarrage du serveur sur le port '+PORT));
+
+
